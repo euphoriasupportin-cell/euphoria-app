e4bb6e53a63f0435c4ee7157c08b00f0a8332b84
Debraj Halder <debrajh13@gmail.com>
2026-09-01 16:55:40 +0530

    Fix encoding and update wsgi

--- app_download/wsgi.py
+++ app_download/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for app_download project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_download.settings')
 
 application = get_wsgi_application()
+
+# 🔴 Vercel-এর জন্য এই লাইনটি আবশ্যক
+app = application
